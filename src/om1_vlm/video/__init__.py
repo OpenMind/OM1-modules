875a3946c59acf803c114d2b5be6b2891e966f06
WenjinFu <wenjinf@alumni.cmu.edu>
2025-08-28 11:58:25 -0700

    update __init__ file

--- src/om1_vlm/video/__init__.py
+++ src/om1_vlm/video/__init__.py
@@ -2,4 +2,4 @@ from .gazebo_video_stream import GazeboVideoStream
 from .video_stream import VideoStream
 from .video_utils import enumerate_video_devices
 
-__all__ = ["enumerate_video_devices", "VideoStream", "GazeboVideoStream"]
+__all__ = ["enumerate_video_devices", "MPVideoStream", "MPVideoStream", "GazeboVideoStream"]
