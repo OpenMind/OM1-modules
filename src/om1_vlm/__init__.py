5de2e4ac6d261270dcd09144abb385150a6f10a5
WenjinFu <wenjinf@alumni.cmu.edu>
2025-08-28 13:33:03 -0700

    update __init__ file

--- src/om1_vlm/__init__.py
+++ src/om1_vlm/__init__.py
@@ -1,12 +1,12 @@
 from .nv_nano_llm import VideoDeviceInput, VideoStreamInput, VLMProcessor
 from .processor import ConnectionProcessor
-from .video import GazeboVideoStream, VideoStream, enumerate_video_devices
+from .video import GazeboVideoStream, MPVideoStream, enumerate_video_devices
 
 __all__ = [
     "VLMProcessor",
     "VideoDeviceInput",
     "VideoStreamInput",
-    "VideoStream",
+    "MPVideoStream",
     "GazeboVideoStream",
     "ConnectionProcessor",
     "enumerate_video_devices",
