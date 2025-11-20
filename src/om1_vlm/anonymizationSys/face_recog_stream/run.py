8dbd7c3d6f86569d96a56be5c0af0bf94bc6a244
Wenjin Fu <wenjinf@alumni.cmu.edu>
2025-11-20 14:28:37 -0800

    fix error

--- src/om1_vlm/anonymizationSys/face_recog_stream/run.py
+++ src/om1_vlm/anonymizationSys/face_recog_stream/run.py
@@ -211,20 +211,18 @@ def main() -> None:
 
     script_dir = os.path.dirname(os.path.abspath(__file__))
     models_dir = os.path.join(script_dir, "..", "models")
-    platform = os.environ.get("OM1_PLATFORM","").lower()
+    platform = os.environ.get("OM1_PLATFORM", "").lower()
     if platform == "thor":
         scrfd_name = "thor_buffalo_m_w600k_r50.engine"
         arc_name = "thor_buffalo_m_w600k_r50.engine"
-    elif platfrom == "orin":
+    elif platform == "orin":
         scrfd_name = "orin_scrfd_2.5g_bnkps_shape640x640.engine"
         arc_name = "orin_buffalo_m_w600k_r50.engine"
     else:
         scrfd_name = "thor_buffalo_m_w600k_r50.engine"
         arc_name = "thor_buffalo_m_w600k_r50.engine"
-        
-    default_scrfd_engine = os.path.join(
-        models_dir, scrfd_name
-    )
+
+    default_scrfd_engine = os.path.join(models_dir, scrfd_name)
     default_arc_engine = os.path.join(models_dir, arc_name)
     default_gallery = os.path.join(script_dir, "..", "gallery")
 
