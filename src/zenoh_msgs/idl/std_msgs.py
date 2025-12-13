45d995651c867ebe55b133f48f2374ab19378567
openmindev <boyuan.eth@gmail.com>
2025-12-13 14:23:05 -0800

    Add pyright

--- src/zenoh_msgs/idl/std_msgs.py
+++ src/zenoh_msgs/idl/std_msgs.py
@@ -48,6 +48,6 @@ def prepare_header(frame_id: str = "") -> Header:
     """
     ts = time.time()
     remainder, seconds = math.modf(ts)
-    timestamp = Time(sec=int32(seconds), nanosec=uint32(remainder * 1000000000))
+    timestamp = Time(sec=int(seconds), nanosec=int(remainder * 1000000000))
     header = Header(stamp=timestamp, frame_id=frame_id)
     return header
